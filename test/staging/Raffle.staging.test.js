2efba157e6a222b2411797f10adeda6d95dafa7c
umairahmed88 <umairahmed_88@ymail.com>
2023-03-04 03:18:21 +0500

    added recent winner

--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -1,10 +1,10 @@
 const { assert, expect } = require("chai");
-const { network, getNamedAccounts, ethers } = require("hardhat");
-const { developmentChains } = require("../../helper-hardhat-config.js");
+const { getNamedAccounts, ethers, network } = require("hardhat");
+const { developmentChains } = require("../../helper-hardhat-config");
 
 developmentChains.includes(network.name)
 	? describe.skip
-	: describe("Raffle", function () {
+	: describe("Raffle Staging Tests", function () {
 			let raffle, raffleEntranceFee, deployer;
 
 			beforeEach(async function () {
@@ -12,16 +12,22 @@ developmentChains.includes(network.name)
 				raffle = await ethers.getContract("Raffle", deployer);
 				raffleEntranceFee = await raffle.getEntranceFee();
 			});
+
 			describe("fulfillRandomWords", function () {
-				it("works with live Chainlink keepers and Chainlink VRF, wwe get a random winner", async function () {
+				it("works with live Chainlink Keepers and Chainlink VRF, we get a random winner", async function () {
+					// enter the raffle
+					console.log("Setting up test...");
 					const startingTimeStamp = await raffle.getLatestTimeStamp();
 					const accounts = await ethers.getSigners();
 
+					console.log("Setting up Listener...");
 					await new Promise(async (resolve, reject) => {
+						// setup listener before we enter the raffle
+						// Just in case the blockchain moves REALLY fast
 						raffle.once("WinnerPicked", async () => {
 							console.log("WinnerPicked event fired!");
-
 							try {
+								// add our asserts here
 								const recentWinner = await raffle.getRecentWinner();
 								const raffleState = await raffle.getRaffleState();
 								const winnerEndingBalance = await accounts[0].getBalance();
@@ -37,13 +43,18 @@ developmentChains.includes(network.name)
 								assert(endingTimeStamp > startingTimeStamp);
 								resolve();
 							} catch (error) {
-								console.error(error);
+								console.log(error);
 								reject(error);
 							}
 						});
-
-						await raffle.enterRaffle({ value: raffleEntranceFee });
+						// Then entering the raffle
+						console.log("Entering Raffle...");
+						const tx = await raffle.enterRaffle({ value: raffleEntranceFee });
+						await tx.wait(1);
+						console.log("Ok, time to wait...");
 						const winnerStartingBalance = await accounts[0].getBalance();
+
+						// and this code WONT complete until our listener has finished listening!
 					});
 				});
 			});

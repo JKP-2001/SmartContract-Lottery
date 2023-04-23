aab5f0b92e7e3eb7297a676c52ef790f5a2857df
JKP-2001 <jkp@Jay>
2023-04-23 22:12:55 +0530

    Final Deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,9 +6,9 @@ require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -19,16 +19,16 @@ module.exports = {
 			chainId: 31337,
 			blockConfirmations: 1,
 		},
-		goerli: {
-			chainId: 5,
-			blockConfirmations: 6,
-			url: GOERLI_RPC_URL,
+		sepolia:{
+			url: SEPOLIA_RPC_URL,
 			accounts: [PRIVATE_KEY],
+			chainId: 11155111,
+			blockConfirmations:6,
 		},
 	},
 	etherscan: {
 		apiKey: {
-			goerli: ETHERSCAN_API_KEY,
+			SEPOLIA: ETHERSCAN_API_KEY,
 		},
 	},
 	gasReporter: {
@@ -42,7 +42,7 @@ module.exports = {
 	solidity: {
 		compilers: [
 			{
-				version: "0.8.17",
+				version: "0.8.18",
 			},
 			{
 				version: "0.4.24",
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,13 +1,13 @@
 const { ethers } = require("hardhat");
 
 const networkConfig = {
-	5: {
-		name: "goerli",
-		vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+	11155111: {
+		name: "sepolia",
+		vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
 		entranceFee: ethers.utils.parseEther("0.01"),
 		gasLane:
-			"0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-		subscriptionId: "10394",
+			"0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+		subscriptionId: "1390",
 		callbackGasLimit: "500000",
 		interval: "30",
 	},
@@ -15,7 +15,7 @@ const networkConfig = {
 		name: "hardhat",
 		entranceFee: ethers.utils.parseEther("0.01"),
 		gasLane:
-			"0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+			"0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
 		callbackGasLimit: "500000",
 		interval: "30",
 	},
